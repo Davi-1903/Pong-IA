3c05ce14281bdf16ed2f29733c37a616a30dc9a6
Davi <franciscodavi327@gmail.com>
2024-12-25 22:37:00 -0300

    Documentar o arquivo principal

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ from lib.classes import *
 
 
 class Pong:
+    '''Jogo Pong'''
     def __init__(self):
+        '''Método construtor'''
         pygame.init()
         self.screen_config()
         self.loop()
     
-    def screen_config(self) -> None:
+    def screen_config(self):
+        '''Método para configuração da tela'''
         self.screen = pygame.display.set_mode((LARGURA, ALTURA))
         pygame.display.set_caption('Pong IA')
         self.clock = pygame.time.Clock()
     
     def loop(self):
+        '''Loop global do jogo'''
         jogo = Game(self.screen)
         while True:
             self.screen.fill('#202020')
@@ -23,6 +27,7 @@ class Pong:
             pygame.display.update()
     
     def eventos(self):
+        '''Método para os eventos'''
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
